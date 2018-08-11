e04916293a7f6511734ba792fc87f914edc0eca5
Venkatram Veerareddy <venkat.veerareddy@hotmail.com>
2018-08-11 18:49:00 +0530

    Create DLinkedList.cpp

---
+++ src/DLinkedList.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct Node{
+  int data;
+  Node *left;
+  Node *right;
+};
+
+class DLinkedList {
+
+  private:
+    Node *head,*tail;
+     public:
+    DList(){
+      head = NULL;
+      tail = NULL;
+    }
+
+    ~DList(){
+      Node *temp = head;
+      while(temp != NULL){
+        Node *nd = temp;
+        temp = temp->right;
+        delete nd;
+      }
+      head = NULL;
+      tail = NULL;
+    }
+  public:
+    void append(int data){
+      //construct node object
+      Node *temp = new Node;
+      temp->data = data;
+      temp->left = NULL;
+      temp->right = NULL;
+
+      if(head == NULL){
+        //head and tail points the newly created node
+        head = temp;
+        tail = temp;
+      }else{  
+        tail->right = temp;
+        temp->left = tail;
+        tail = temp;//now tail points the temp, means it moves
+      }
+    }
+
+    void displayForward(){
+      Node *temp = head;
+      while(temp != NULL){
+        cout<<temp->data<<endl;
+        temp = temp->right;
+      }
+    }
+
+    void displayBackward(){
+      Node *temp = tail;
+      while(temp != NULL){
+        cout<<temp->data<<endl;
+        temp = temp->left;
+      }
+    }
+
+};
+
+int main() {
+  DList list;
+  list.append(4);
+  list.append(10);
+  list.append(12);
+  list.append(15);
+  list.append(18);
+  list.append(3);
+  cout<<"forward display"<<endl;
+  list.displayForward();
+  cout<<"backward display"<<endl;
+  list.displayBackward();
+}
