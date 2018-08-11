30732c16a1e582c38c5b92756b4d479a5e1ceadd
Venkatram Veerareddy <venkat.veerareddy@hotmail.com>
2018-08-11 23:01:30 +0530

    Update DLinkedList.cpp

--- src/DLinkedList.cpp
+++ src/DLinkedList.cpp
@@ -13,7 +13,7 @@ class DList {
 
   private:
     Node *head,*tail;
-  
+
   public:
     DList(){
       head = NULL;
@@ -27,8 +27,6 @@ class DList {
         temp = temp->right;
         delete nd;
       }
-      head = NULL;
-      tail = NULL;
     }
     //adding at end
     void append(int data){
@@ -61,6 +59,16 @@ class DList {
       head = temp;
 
     }
+    //finding the length of list
+    int length(){
+      Node* current = head; 
+      int count = 0; 
+      while(current != NULL){
+        current = current->right;
+        count++;
+      }
+      return count;
+    }
 
     //deleting the node
     void deleteElement(int value){
@@ -74,12 +82,13 @@ class DList {
           delete current;
           return;
         }
-        //case 2: middle element
+        
         Node* previous = head;
         current = head->right;
         while(current != NULL){
           if(current->data == value){
             cout<<"Deleting the element"<<endl;
+            //case 2: middle element
             if(current->right != NULL){
               previous->right = current->right;
               Node* next = current->right;
@@ -120,6 +129,67 @@ class DList {
 };
 
 int main() {
+
+  int choice,data;
+  DList list;
+
+  while(1){
+    cout<<endl<<"----------------------------"<<endl;
+    cout<<endl<<"Operations on Doubly linked list"<<endl;
+    cout<<endl<<"----------------------------"<<endl; 
+    cout<<"1.Add at endining"<<endl;
+    cout<<"2.Add at beginning"<<endl;
+    cout<<"3.Delete"<<endl;
+    cout<<"4.Forward Display"<<endl;
+    cout<<"5.Backward Display"<<endl;
+    cout<<"6.Length of double linked list"<<endl;
+    cout<<"7.Quit"<<endl;
+    cout<<"Enter your choice : ";
+    cin>>choice;
+
+    if(choice == 7){
+      return 0;
+    }
+    switch ( choice ){
+      case 1:
+          cout<<"Enter the element: ";
+          cin>>data;
+          list.append(data);
+          cout<<endl;
+          break;
+      case 2:
+          cout<<"Enter the element: ";
+          cin>>data;
+          list.prepend(data);
+          cout<<endl;
+          break;
+      case 3:
+          cout<<"Enter the element to delete: ";
+          cin>>data;
+          list.deleteElement(data);
+          cout<<endl;
+          break;
+      case 4:
+          list.displayForward();
+          cout<<endl;
+          break;
+      case 5:
+          list.displayBackward();
+          cout<<endl;
+          break;
+      case 6:
+          cout<<list.length();
+          cout<<endl;
+          break;
+      default:
+          cout<<"Wrong choice"<<endl;
+    }
+  }
+  return 0;
+
+}
+/*
+int main2() {
   DList list;
   list.append(4);
   list.append(10);
@@ -134,22 +204,23 @@ int main() {
   list.displayBackward();
   cout<<"deleting element "<<endl;
   //testing deleting
-  /*list.deleteElement(199);
+  list.deleteElement(199);
   cout<<"forward display"<<endl;
   list.displayForward();
   cout<<"backward display"<<endl;
   list.displayBackward();
-  */
+ 
   //testing deleting for middle element
-  /*list.deleteElement(15);
+  list.deleteElement(15);
   cout<<"forward display"<<endl;
   list.displayForward();
   cout<<"backward display"<<endl;
-  list.displayBackward();*/
+  list.displayBackward();
 
   list.deleteElement(3);
   cout<<"forward display"<<endl;
   list.displayForward();
   cout<<"backward display"<<endl;
   list.displayBackward();
-}
+  
+}*/
